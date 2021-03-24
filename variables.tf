1feeec5d1075ce3bf5cd8f88fafb0e84921d7997
Your Name <you@example.com>
2021-03-24 10:39:49 -0500

    Added region

--- variables.tf
+++ variables.tf
@@ -3,6 +3,11 @@ variable "prefix" {
   default     = "stenio"
 }
 
+variable "region" {
+  description = "AWS Region"
+  default     = "us-east-1"
+}
+
 variable "tags" {
   description = "Prefix added to unique bucket name"
   default     = {
